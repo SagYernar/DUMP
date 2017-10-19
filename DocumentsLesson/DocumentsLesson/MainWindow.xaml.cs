ecd6d0b54b78350f7d9e9d3e22a5f15758601c26
SagYernar <sagyernar@gmail.com>
2017-10-19 18:13:27 +0600

    !

---
+++ DocumentsLesson/DocumentsLesson/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
+
+namespace DocumentsLesson
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //XpsDocument doc = new XpsDocument("12.xps", FileAccess.Write);
+            //XpsDocumentWriter writer = XpsDocument.CreateXpsDocumentWriter(doc);
+            //writer.Write(fixeddd.Document.DocumentPaginator);
+            //doc.Close();
+
+            XpsDocument document = new XpsDocument("12.xps", FileAccess.Read);
+            fixeddd.Document = document.GetFixedDocumentSequence();
+            document.Close();
+        }
+
+        
+    }
+}
